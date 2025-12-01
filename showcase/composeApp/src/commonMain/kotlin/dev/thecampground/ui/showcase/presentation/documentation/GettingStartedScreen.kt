5f53cec81d8ab347246463dfc00747ff5760efa0
brys0 <brystn@proton.me>
2025-11-30 19:08:29 -0600

    feat: Add stubs for getting started and styling screens

---
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/GettingStartedScreen.kt
@@ -0,0 +1,14 @@
+package dev.thecampground.ui.showcase.presentation.documentation
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.ScreenKey
+import cafe.adriel.voyager.core.screen.uniqueScreenKey
+
+class GettingStartedScreen : Screen {
+    override val key: ScreenKey = uniqueScreenKey
+    @Composable
+    override fun Content() {
+
+    }
+}
---
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/StylingScreen.kt
@@ -0,0 +1,11 @@
+package dev.thecampground.ui.showcase.presentation.documentation
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class StylingScreen : Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
