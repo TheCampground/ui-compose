7f187495f8fdfda16bc58abff069b15e52bda5db
brys0 <brystn@proton.me>
2025-11-30 19:03:23 -0600

    feat: Add transition support for showcase app

--- showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/App.kt
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/App.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
 import dev.thecampground.ui.Colors
+import dev.thecampground.ui.showcase.presentation.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -13,8 +16,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme(typography = spaceGroteskTypography()) {
         Box(Modifier.background(Colors.BG_ALT)) {
-//            Home()
-            Docs()
+            Navigator(HomeScreen()) { nav ->
+                SlideTransition(nav)
+            }
         }
     }
 }
