81a17f68830df1bf702a64b093a2baf3c8da4e3f
brys0 <brystn@proton.me>
2025-11-30 19:06:04 -0600

    feat: Add automatically generated components detail screen

---
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/components/ComponentDetailsScreen.kt
@@ -0,0 +1,174 @@
+package dev.thecampground.ui.showcase.presentation.documentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.ScreenKey
+import camgroundui.showcase.composeapp.generated.resources.Res
+import camgroundui.showcase.composeapp.generated.resources.campground
+import dev.snipme.highlights.DefaultHighlightsResultListener
+import dev.snipme.highlights.Highlights
+import dev.snipme.highlights.model.CodeHighlight
+import dev.snipme.highlights.model.SyntaxLanguage
+import dev.snipme.highlights.model.SyntaxTheme
+import dev.snipme.highlights.model.SyntaxThemes
+import dev.thecampground.ui.Alert
+import dev.thecampground.ui.AlertVariants
+import dev.thecampground.ui.Button
+import dev.thecampground.ui.ButtonVariants
+import dev.thecampground.ui.CampgroundUIDocDefinitions
+import dev.thecampground.ui.Colors
+import dev.thecampground.ui.InputSizes
+import dev.thecampground.ui.RoundedInputShape
+import dev.thecampground.ui.RoundedInputShapeFull
+import dev.thecampground.ui.RoundedPreviewShape
+import dev.thecampground.ui.internal.CampgroundUIExamples
+import dev.thecampground.ui.showcase.internal.generateAnnotatedString
+import dev.thecampground.ui.showcase.presentation.documentation.DocumentationRoot
+import dev.thecampground.ui.showcase.sourceCodeProFamily
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+data class ComponentDetailsScreen(val component: String) : Screen {
+    override val key: ScreenKey = component
+
+    @Composable
+    override fun Content() {
+        val component = CampgroundUIDocDefinitions.componentDefinitions[component]?.first()
+        val scrollState = rememberScrollState()
+
+        BoxWithConstraints(modifier = Modifier.verticalScroll(scrollState)) {
+            if (component == null) {
+                Text("A component was mentioned but it's documentation could not be found.")
+            } else {
+                DocumentationRoot(
+                    name = component.name,
+                    description = component.description
+                ) {
+
+                    val firstExample = CampgroundUIExamples.exampleDefinitions[component.uniqueName]
+                    if (firstExample != null) {
+
+                        val highlights by remember {
+                            mutableStateOf(
+                                Highlights.Builder(
+                                    code = firstExample,
+                                    language = SyntaxLanguage.KOTLIN,
+                                    theme = SyntaxTheme(
+                                        key = "VESPER_LIGHT",
+                                        code = 0x00000,
+                                        keyword = 0x505050,
+                                        string = 0x008080,
+                                        literal = 0xFF8C00,
+                                        comment = 0x8B8B8B,
+                                        metadata = 0xFF8C00,
+                                        multilineComment = 0x8B8B8B,
+                                        punctuation = 0x505050,
+                                        mark = 0xFF8C00,
+                                    )
+                                ).build()
+                            )
+                        }
+                        var currentText by remember { mutableStateOf(AnnotatedString("")) }
+
+                        LaunchedEffect(highlights) {
+                            highlights.getHighlightsAsync(object :
+                                DefaultHighlightsResultListener() {
+                                override fun onSuccess(result: List<CodeHighlight>) {
+                                    currentText = result.generateAnnotatedString(firstExample)
+                                }
+                            })
+                        }
+
+                        Column(
+                            modifier = Modifier.fillMaxWidth( if (maxWidth < 800.dp) 1f else .8f).clip(RoundedPreviewShape).border(
+                                1.dp,
+                                color = Colors.BORDER.copy(alpha = 0.2f),
+                                shape = RoundedPreviewShape
+                            )
+                        ) {
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.background(Colors.BG).padding(32.dp)
+                                    .fillMaxWidth()
+                                    .defaultMinSize(minHeight = 128.dp)
+                            ) {
+                                component.example?.let { it() }
+                            }
+
+                            Column(
+                                modifier = Modifier.fillMaxWidth().background(Colors.BG_SECONDARY)
+                            ) {
+                                Column {
+                                    HorizontalDivider(color = Colors.BORDER.copy(alpha = 0.1f))
+                                    Row(
+                                        modifier = Modifier.background(Colors.GHOST_BUTTON_HOVERED)
+                                            .fillMaxWidth().padding(horizontal = 12.dp),
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        horizontalArrangement = Arrangement.SpaceBetween
+                                    ) {
+                                        Text("Composable.kt")
+
+                                        Button(
+                                            onClick = {},
+                                            variant = ButtonVariants.GHOST,
+                                            size = InputSizes.ICON,
+                                            icon = { tint, size ->
+                                                Icon(
+                                                    painterResource(Res.drawable.campground),
+                                                    contentDescription = "",
+                                                    tint = tint,
+                                                    modifier = Modifier.size(size)
+                                                )
+                                            },
+                                            content = null
+                                        )
+                                    }
+                                    HorizontalDivider(color = Colors.BORDER.copy(alpha = 0.1f))
+                                }
+                                Text(
+                                    currentText,
+                                    fontFamily = sourceCodeProFamily(),
+                                    modifier = Modifier.padding(
+                                        horizontal = 12.dp,
+                                        vertical = 14.dp
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
