89d4c544384626179f89312148ee9ef2333e3137
Bryson T. <35676267+brys0@users.noreply.github.com>
2025-12-04 12:58:05 -0600

    feat: Add alternative rounded preview shape

--- library/src/commonMain/kotlin/dev/thecampground/ui/Defaults.kt
+++ library/src/commonMain/kotlin/dev/thecampground/ui/Defaults.kt
@@ -8,6 +8,8 @@ val InputPaddingValues = PaddingValues(vertical = 7.dp, horizontal = 12.dp)
 val InputPaddingValuesSmall = PaddingValues(vertical = 4.dp, horizontal = 8.dp)
 val InputPaddingValuesIcon = PaddingValues(8.dp)
 
+val RoundedPreviewShape = RoundedCornerShape(14.dp)
+
 val RoundedInputShape = RoundedCornerShape(10.dp)
 val RoundedInputShapePill = RoundedCornerShape(16.dp)
 val RoundedInputShapeFull = RoundedCornerShape(32.dp)
