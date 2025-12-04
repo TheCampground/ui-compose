8616636767efeb1f14deeb70ccc54f3575822464
Bryson T. <35676267+brys0@users.noreply.github.com>
2025-12-04 12:58:04 -0600

    feat: Add stringified example function processor

--- annotation/src/commonMain/kotlin/dev/thecampground/ui/annotation/Annotations.kt
+++ annotation/src/commonMain/kotlin/dev/thecampground/ui/annotation/Annotations.kt
@@ -2,12 +2,16 @@ package dev.thecampground.ui.annotation
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.SOURCE)
-annotation class CampgroundUIComponent(val description: String = "No description provided")
+annotation class CampgroundUIComponent(val uniqueName: String, val description: String = "No description provided")
 
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class CampgroundUIComponentExample
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
-annotation class CampgroundUIType()
+annotation class CampgroundUIType
 
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.SOURCE)
