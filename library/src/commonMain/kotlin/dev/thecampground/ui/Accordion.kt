40fa3b0a4ffd36319d1324e0ce7fb19c468d4be1
brys0 <35676267+brys0@users.noreply.github.com>
2025-12-29 17:24:37 -0600

    feat: Add accordion component
    Closes #1

--- library/src/commonMain/kotlin/dev/thecampground/ui/Accordion.kt
+++ library/src/commonMain/kotlin/dev/thecampground/ui/Accordion.kt
@@ -2,53 +2,219 @@ package dev.thecampground.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import camgroundui.library.generated.resources.Res
+import camgroundui.library.generated.resources.caret_up
+import dev.thecampground.ui.annotation.CampgroundComponent
+import dev.thecampground.ui.annotation.CampgroundType
+import org.jetbrains.compose.resources.painterResource
+import kotlin.random.Random
+
+@CampgroundType
+typealias AccordionActionScope = @Composable (id: String, controller: AccordionController) -> Unit
+
+class AccordionController(
+    private val multiOpen: Boolean
+) {
+    private val itemStates =
+        mutableStateMapOf<String, MutableState<Boolean>>().withDefault { mutableStateOf(false) }
+
+    fun registerItem(id: String) {
+        itemStates[id] = mutableStateOf(false)
+    }
+
+    fun isOpen(id: String): State<Boolean> =
+        itemStates.getValue(id)
+
+    fun toggle(id: String) {
+        val current = itemStates.getValue(id)
+
+        val newValue = !current.value
+        current.value = newValue
+
+        if (!multiOpen && newValue) {
+            // close all other open items
+            itemStates.forEach { (otherId, state) ->
+                if (otherId != id && state.value) {
+                    state.value = false
+                }
+            }
+        }
+    }
+}
+
+val LocalAccordionController = staticCompositionLocalOf<AccordionController> {
+    error("AccordionItem must be inside AccordionRoot")
+}
 
 @Composable
-fun AccordionRoot(type: AccordionTypes = AccordionTypes.SINGLE, content: @Composable () -> Unit) {
-    var visible by remember { mutableStateOf(false) }
-    Box() {
-        Column {
+@CampgroundComponent(description = "A custom button components with multiple variations and sizes")
+fun AccordionRoot(
+    type: AccordionTypes = AccordionTypes.SINGLE,
+    content: @Composable () -> Unit
+) {
+    val controller = remember { AccordionController(type == AccordionTypes.MULTIPLE) }
 
-
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                Text("What is the campground")
-
-                Button(onClick = {
-                    visible = !visible
-                }, text = "O")
-            }
-
-
-
-            AnimatedVisibility(visible = visible, enter = fadeIn(spring(stiffness = Spring.StiffnessMedium)) + expandVertically(clip = false, animationSpec = spring(stiffness = Spring.StiffnessMedium)), exit = fadeOut(animationSpec = spring(stiffness = Spring.StiffnessMedium)) + shrinkVertically(clip = false, animationSpec = spring(stiffness = Spring.StiffnessMedium))) {
-                Text("Campground is an independent collective where creativity meets code and design.")
-            }
-
-            Box(modifier = Modifier.fillMaxWidth().height(1.dp).background(Color.Black))
+    Column(verticalArrangement = Arrangement.spacedBy(18.dp)) {
+        CompositionLocalProvider(
+            LocalAccordionController provides controller
+        ) {
+            content()
         }
+    }
+}
+
+@Composable
+@CampgroundComponent(description = "A custom button components with multiple variations and sizes")
+fun BaseAccordionItem(
+    id: String = remember { Random.nextBytes(24).toString() },
+    enabled: Boolean = true,
+    action: AccordionActionScope = { id, controller ->
+        Button(
+            variant = ButtonVariants.GHOST,
+            size = InputSizes.ICON,
+            onClick = { controller.toggle(id) })
+        { tint ->
+            val isOpen = LocalAccordionController.current.isOpen(id)
+            val rotateState by animateFloatAsState(if (isOpen.value) 180f else 0f)
+
+            Icon(
+                painterResource(Res.drawable.caret_up),
+                contentDescription = "Caret",
+                tint = tint,
+                modifier = Modifier.rotate(rotateState).size(18.dp)
+            )
+        }
+    },
+    title: @Composable () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    val controller = LocalAccordionController.current
+    val theme = LocalCampgroundTheme.current
+    val interactionSource = remember { MutableInteractionSource() }
+
+    // Register this item so the controller knows it
+    LaunchedEffect(id) {
+        controller.registerItem(id)
+    }
+
+    val isOpen by controller.isOpen(id)
+
+    Column(
+        modifier = Modifier.clickable(
+            enabled = enabled,
+            interactionSource = interactionSource,
+            indication = null,
+            role = Role.DropdownList
+        ) {
+            controller.toggle(id)
+        },
+//        verticalArrangement = Arrangement.spacedBy(18.dp)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            title()
+
+            action(id, controller)
+        }
+
+        AnimatedVisibility(
+            visible = isOpen,
+            enter = fadeIn(spring(stiffness = Spring.StiffnessMedium)) + expandVertically(
+                clip = false,
+                animationSpec = spring(stiffness = Spring.StiffnessMedium)
+            ),
+            exit = fadeOut(animationSpec = spring(stiffness = Spring.StiffnessMedium)) + shrinkVertically(
+                clip = false,
+                animationSpec = spring(stiffness = Spring.StiffnessMedium)
+            ),
+        ) {
+            content()
+        }
+
+        Box(
+            modifier = Modifier.padding(top = 12.dp).fillMaxWidth().height(1.dp)
+                .background(theme.inverse.copy(alpha = 0.2f))
+        )
+    }
+}
+
+@Composable
+fun AccordionItem(
+    id: String = remember { Random.nextBytes(24).toString() },
+    action: AccordionActionScope = { id, controller ->
+        Button(
+            variant = ButtonVariants.GHOST,
+            size = InputSizes.ICON,
+            onClick = { controller.toggle(id) })
+        { tint ->
+            val isOpen = LocalAccordionController.current.isOpen(id)
+            val rotateState by animateFloatAsState(if (isOpen.value) 180f else 0f)
+
+            Icon(
+                painterResource(Res.drawable.caret_up),
+                contentDescription = "Caret",
+                tint = tint,
+                modifier = Modifier.rotate(rotateState).size(18.dp)
+            )
+        }
+    },
+    title: String,
+    content: String
+) {
+    val theme = LocalCampgroundTheme.current
+
+    BaseAccordionItem(
+        id,
+        action = action,
+        title = {
+            Text(
+                title,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                letterSpacing = (-0.4).sp,
+                color = theme.text.default
+            )
+        },
+    ) {
+        Text(
+            content,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = (-0.4).sp,
+            color = theme.text.secondary
+        )
     }
 }
---
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/internal/components/Accordion.kt
@@ -0,0 +1,58 @@
+package dev.thecampground.ui.showcase.internal.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import dev.thecampground.ui.AccordionItem
+import dev.thecampground.ui.AccordionRoot
+import dev.thecampground.ui.AccordionTypes
+import dev.thecampground.ui.LocalCampgroundTheme
+import dev.thecampground.ui.showcase.domain.CampgroundCodeExample
+import dev.thecampground.ui.showcase.presentation.documentation.DocumentationRoot
+
+private val accordionExample = CampgroundCodeExample(
+    code = """
+         AccordionRoot(type = AccordionTypes.SINGLE) {
+               AccordionItem(
+                  title = "What is Campground?",
+                  content = "Campground is an independent collective where creativity meets code and design."
+               )
+               AccordionItem(
+                 title = "What is The Campsite?", 
+                 content = "The Campsite is an ever-growing community on Discord that aims to provide a warm welcome to all new members joining us!"
+               )
+         }
+    """.trimIndent()
+) {
+    AccordionRoot(type = AccordionTypes.SINGLE) {
+        AccordionItem(
+            title = "What is Campground?",
+            content = "Campground is an independent collective where creativity meets code and design."
+        )
+        AccordionItem(
+            title = "What is The Campsite?",
+            content = "The Campsite is an ever-growing community on Discord that aims to provide a warm welcome to all new members joining us!"
+        )
+    }
+}
+
+@Composable
+fun AccordionDocumentation(maxWidth: Dp) {
+    val theme = LocalCampgroundTheme.current
+
+    DocumentationRoot(
+        name = "Accordion",
+        "Organizes content into collapsible sections.",
+        theme = theme
+    ) {
+//        ComponentCodePreview(maxWidth, CampgroundUIExamples.ButtonExample1) {
+//            ButtonExample1()
+//        }
+//        ComponentPreview(title ="Variants", description = "The different button variants", contentString = CampgroundUIExamples.ButtonExample1) {
+//
+//        }
+
+        ComponentCodePreview(maxWidth, accordionExample.code) {
+            accordionExample.component()
+        }
+    }
+}
