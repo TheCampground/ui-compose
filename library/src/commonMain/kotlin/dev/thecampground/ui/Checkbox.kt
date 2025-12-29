7a545fa1a13231d7e2c88c86884849b11467d298
brys0 <35676267+brys0@users.noreply.github.com>
2025-12-29 17:38:01 -0600

    feat: Create checkbox stub

---
+++ library/src/commonMain/kotlin/dev/thecampground/ui/Checkbox.kt
@@ -0,0 +1,18 @@
+package dev.thecampground.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun BaseCheckbox() {
+
+    Box(modifier = Modifier.background(Color.Transparent)) {
+
+    }
+
+}
