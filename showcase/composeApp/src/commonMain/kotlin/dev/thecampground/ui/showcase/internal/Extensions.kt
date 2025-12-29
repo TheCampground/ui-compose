beb13e08377d8b905b80b002b18005b148d3f590
brys0 <35676267+brys0@users.noreply.github.com>
2025-12-29 17:37:52 -0600

    feat: Allow real time event listeners in navigation
    This commit allows the buttons to know which page is currently active, and which is not.

--- showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/internal/Extensions.kt
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/internal/Extensions.kt
@@ -1,12 +1,38 @@
 package dev.thecampground.ui.showcase.internal
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.Navigator
 import dev.snipme.highlights.model.BoldHighlight
 import dev.snipme.highlights.model.CodeHighlight
 import dev.snipme.highlights.model.ColorHighlight
+
+
+private val screenListeners: MutableList<(Screen) -> Unit> = mutableListOf()
+
+
+fun Navigator.addOnScreenChangedListener(listener: (Screen) -> Unit) {
+    println("Added listener: ${listener}")
+    screenListeners += listener
+}
+
+fun Navigator.removeOnScreenChangedListener(listener: (Screen) -> Unit) {
+    screenListeners -= listener
+}
+
+fun Navigator.safePush(screen: Screen) {
+    println("Safe pushed: ${screenListeners}")
+    push(screen)
+    screenListeners.forEach { it.invoke(screen) }
+}
 
 fun List<CodeHighlight>.generateAnnotatedString(code: String) =
     buildAnnotatedString {
@@ -28,3 +54,26 @@ fun List<CodeHighlight>.generateAnnotatedString(code: String) =
             }
         }
     }
+
+@Composable
+fun Navigator.currentScreenAsState(): State<Screen> {
+    // Recompose when navigator.lastItem changes
+    val state = remember { mutableStateOf(lastItem) }
+
+    // Voyager NavigatorContent is what triggers recompositions.
+    // We hook into it using this trick:
+    DisposableEffect(this) {
+        val listener: (Screen) -> Unit = { newScreen ->
+            state.value = newScreen
+        }
+
+        // Watch navigation changes
+        this@currentScreenAsState.addOnScreenChangedListener(listener)
+
+        onDispose {
+            this@currentScreenAsState.removeOnScreenChangedListener(listener)
+        }
+    }
+
+    return state
+}
