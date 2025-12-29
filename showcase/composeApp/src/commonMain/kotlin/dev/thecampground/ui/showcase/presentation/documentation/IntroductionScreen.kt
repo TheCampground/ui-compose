94db91dc339e3daedec2a15613c6e76b91b25557
brys0 <35676267+brys0@users.noreply.github.com>
2025-12-29 17:36:46 -0600

    chore: remove unnecessary imports

--- showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/IntroductionScreen.kt
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/IntroductionScreen.kt
@@ -1,17 +1,11 @@
 package dev.thecampground.ui.showcase.presentation.documentation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
-import dev.thecampground.ui.Colors
 import dev.thecampground.ui.LocalCampgroundTheme
 import dev.thecampground.ui.showcase.presentation.defaultLink
 
