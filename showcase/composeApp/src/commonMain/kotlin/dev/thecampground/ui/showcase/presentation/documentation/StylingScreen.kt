5b9d05583513afd268a33b4c11f97febb0f87da7
brys0 <35676267+brys0@users.noreply.github.com>
2025-12-29 17:30:17 -0600

    fix: Styling page now renders

--- showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/StylingScreen.kt
+++ showcase/composeApp/src/commonMain/kotlin/dev/thecampground/ui/showcase/presentation/documentation/StylingScreen.kt
@@ -1,11 +1,12 @@
 package dev.thecampground.ui.showcase.presentation.documentation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
 
 class StylingScreen : Screen {
     @Composable
     override fun Content() {
-
+        Text("Styling")
     }
 }
